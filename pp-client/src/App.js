d938a16bec9f69a99dda7e43470b4b6ce5a9aac0
harshasalim <harshasalim@yahoo.com>
2020-05-10 16:11:04 +0530

    Signup

--- pp-client/src/App.js
+++ pp-client/src/App.js
@@ -2,34 +2,31 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+import themeFile from './util/theme';
+import jwtDecode from 'jwt-decode';
 
 //Components
 import Navbar from './components/Navbar';
+import AuthRoute from './util/AuthRoute';
 
 //Pages
 import home from './pages/home';
 import login from './pages/login';
 import signup from './pages/signup';
 
-const theme= createMuiTheme({
-  palette: {
-    primary: {
-      light: '#d81b60',
-      main: '#f50057',
-      dark: '#bb002f',
-      contrastText: '#fff'
-    },
-    secondary: {
-      light: '#9c4dcc',
-      main: '#6a1b9a',
-      dark: '#38006b',
-      contrastText: '#fff'
-    }
-  },
-  typography: {
-    useNextVariants: true
+const theme= createMuiTheme(themeFile);
+
+let authenticated;
+const token = localStorage.FBIdToken;
+if(token){
+  const decodedToken = jwtDecode(token);
+  if(decodedToken.exp * 1000 < Date.now()){
+    window.location.href='/login'
+    authenticated=false;
+  }else{
+    authenticated=true;
   }
-});
+}
 
 function App() {
   return (
@@ -40,8 +37,8 @@ function App() {
         <div className='container'>
         <Switch>
           <Route exact path="/" component={home}/>
-          <Route exact path="/login" component={login}/>
-          <Route exact path="/signup" component={signup}/>
+          <AuthRoute exact path="/login" component={login} authenticated={authenticated}/>
+          <AuthRoute exact path="/signup" component={signup} authenticated={authenticated}/>
         </Switch>
         </div>
       </Router>
---
+++ pp-client/src/util/theme.js
@@ -0,0 +1,46 @@
+export default {
+    palette: {
+        primary: {
+          light: '#d81b60',
+          main: '#f50057',
+          dark: '#bb002f',
+          contrastText: '#fff',
+        },
+        secondary: {
+          light: '#9c4dcc',
+          main: '#6a1b9a',
+          dark: '#38006b',
+          contrastText: '#fff',
+        }
+      },
+      formFeatures: {
+        form: {
+          textAlign: 'center',
+        },
+        image: {
+            margin: '20px auto 5px auto',
+        },
+        pageTitle: {
+            margin: '10px auto 10px auto',
+        },
+        textField: {
+            margin: '10px auto 10px auto',
+        },
+        button: {
+            marginTop: 20,
+            position: 'relative',
+        },
+        customError: {
+            color: 'red',
+            fontSize: '0.8rem',
+            marginTop: 10,
+        },
+        progress: {
+            position: 'absolute',
+        },
+        typography: {
+          useNextVariants: true,
+        }
+      },
+    
+}

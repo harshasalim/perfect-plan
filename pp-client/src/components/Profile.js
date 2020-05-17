a320eca03ace618ff3815862f2c34cd88b48b9d0
harshasalim <harshasalim@yahoo.com>
2020-05-17 19:51:34 +0530

    Edit profile functionalities

--- pp-client/src/components/Profile.js
+++ pp-client/src/components/Profile.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import withStyles from '@material-ui/core/styles/withStyles';
 import { Link } from 'react-router-dom';
 import dayjs from 'dayjs';
+import EditDetails from './EditDetails';
+import MyButton from '../util/MyButton';
 
 //REDUX
 import { connect } from 'react-redux';
@@ -12,14 +14,13 @@ import Button from '@material-ui/core/Button';
 import { Paper } from '@material-ui/core';
 import MuiLink from '@material-ui/core/Link';
 import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import Tooltip from '@material-ui/core/Tooltip';
 
 //Icons
 import LocationOn from "@material-ui/icons/LocationOn";
 import LinkIcon from "@material-ui/icons/Link";
 import CalendarToday from "@material-ui/icons/CalendarToday";
 import EditIcon from "@material-ui/icons/Edit";
+import KeyboardReturn from '@material-ui/icons/KeyboardReturn';
 
 const styles = (theme) => ({
     paper: {
@@ -83,6 +84,10 @@ class Profile extends Component {
       fileInput.click();
     }
 
+    handleLogout = () => {
+      this.props.logoutUser();
+    }
+
     render() {
         const { classes, 
                 user: { 
@@ -98,11 +103,10 @@ class Profile extends Component {
                     <div className="image-wrapper">
                         <img src={imageUrl} alt="profile" className="profile-image"/>
                         <input type="file" id="imageInput" hidden="hidden" onChange={this.handleImageChange}/>
-                        <Tooltip title="Edit profile picture" placement="top">
-                        <IconButton onClick={this.handleEditPicture} className="button">
-                          <EditIcon color="primary" />
-                        </IconButton>
-                        </Tooltip>
+                       
+                        <MyButton tip="Edit profile picture" onClick={this.handleEditPicture} btnClassName="button">
+                        <EditIcon color='primary' />
+                        </MyButton>
                     </div>
                     <hr />
                     <div className="profile-details">
@@ -114,13 +118,13 @@ class Profile extends Component {
                         <hr />
                         {location && (
                             <Fragment>
-                            <LocationOn color="primary"/> <span>{location}</span>
+                            <LocationOn color="secondary"/> <span>{location}</span>
                             <hr />
                             </Fragment>
                         )}
                         {website && (
                             <Fragment>
-                                <LinkIcon color="primary" />
+                                <LinkIcon color="secondary" />
                                 <a href={website} target="_blank" rel="noopener noreferrer">
                                     {' '}
                                     {website}
@@ -128,9 +132,14 @@ class Profile extends Component {
                                 <hr />
                             </Fragment>
                         )}
-                        <CalendarToday color="primary"/>{' '}
+                        <CalendarToday color="secondary"/>{' '}
                         <span>Joined {dayjs(createdAt).format('MMM YYYY')}</span>
                     </div>
+    
+                    <MyButton tip="Logout" onClick={this.handleLogout} >
+                        <KeyboardReturn color='primary' />
+                        </MyButton>
+                    <EditDetails />
                 </div>
             </Paper>
         ) : (
